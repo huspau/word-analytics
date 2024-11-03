18c7c11a3dc75e7bfe72ff1020ad4082a0982771
Paul Husek <paulhusek@hotmail.com>
2024-11-03 18:40:58 -0500

    Better textarea

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,3 +1,12 @@
 export default function Textarea() {
-  return <textarea className="textarea" />;
+  return (
+    <textarea
+      onChange={(e) => {
+        console.log(e.target.value);
+      }}
+      className="textarea"
+      placeholder="Enter text here..."
+      spellCheck="false"
+    />
+  );
 }
