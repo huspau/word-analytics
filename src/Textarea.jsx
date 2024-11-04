4023c2fac71b27f83bac7b9290570296db3d7d0a
Paul Husek <paulhusek@hotmail.com>
2024-11-03 21:31:45 -0500

    Added basic text validation

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 export default function Textarea() {
+  const [text, setText] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    setShowWarning(newText.includes("<script>"));
+    setText(newText);
+  };
+
   return (
-    <textarea
-      onChange={(e) => {
-        console.log(e.target.value);
-      }}
-      className="textarea"
-      placeholder="Enter text here..."
-      spellCheck="false"
-    />
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter text here..."
+        spellCheck="false"
+      />
+      {showWarning && <Warning />}
+    </div>
   );
 }
